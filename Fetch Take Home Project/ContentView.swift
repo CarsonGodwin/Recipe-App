b284fc281ec0ced62e571a15d32a9bf701ed67c8
CarsonGodwin <carson@carsongodwin.com>
2025-01-30 13:53:46 -0500

    Initial Commit

---
+++ Fetch Take Home Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch Take Home Project
+//
+//  Created by Carson Godwin on 1/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch Take Home ProjectTests/Fetch_Take_Home_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_Home_ProjectTests.swift
+//  Fetch Take Home ProjectTests
+//
+//  Created by Carson Godwin on 1/30/25.
+//
+
+import Testing
+@testable import Fetch_Take_Home_Project
+
+struct Fetch_Take_Home_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
